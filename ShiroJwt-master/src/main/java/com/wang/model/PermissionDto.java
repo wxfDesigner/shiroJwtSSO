7a0be9f3b45bfd3729c3483281e08d208ffb5af7
wxfVeryGood <15821434334@163.com>
2019-03-11 16:49:11 +0800

    commit master version

---
+++ ShiroJwt-master/src/main/java/com/wang/model/PermissionDto.java
@@ -0,0 +1,14 @@
+package com.wang.model;
+
+import com.wang.model.entity.Permission;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Wang926454
+ * @date 2018/8/30 10:48
+ */
+@Table(name = "permission")
+public class PermissionDto extends Permission {
+
+}
